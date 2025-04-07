8389fcbcd812eb536b28d39a3a3401e3a5ebe58d
MEGA <MEGA@gmail.com>
2025-04-07 13:26:29 +0300

    Add project files.

---
+++ Conosle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conosle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 3, 7, 3, 9, 5, 1, 7, 2 };
+            HashSet<int> uniqueNumbers =new HashSet<int>(numbers);
+            Console.WriteLine("Unikalni cisla");
+            foreach(int num in uniqueNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
